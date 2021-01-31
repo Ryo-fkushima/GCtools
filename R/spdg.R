e9fbfec5b71be6aa6ae593c662b9a367ebef90e9
福島諒 <Ryo Fukushima>
2021-01-31 23:26:56 +0900

    commit

--- R/spdg.R
+++ R/spdg.R
@@ -3,22 +3,21 @@
 ########## Parameters ##########
 #elist <- c("La","Ce","Pr","Nd","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu")## elements of interest
 #elist <- c("Cs","Rb","Ba","Th","U","Nb","K","La","Ce","Pb","Pr","Sr","P","Nd","Zr","Sm","Eu","Ti","Dy","Y","Yb","Lu")## elements of interest
-#s <- external## compositional matrix of a natural sample (dataframe) (ppm)
+#s <- example_data_GCtools## compositional matrix of a natural sample (dataframe) (ppm)
 #nml <- "PM_MS95"## "NMORB_SM89", "EMORB_SM89", "OIB_SM89", "CI_MS95", "PM_MS95"
-#output <- T
+#output <- F
 
 
 spdg <- function(elist, s, nml, output){
 
 
 ########## Warning ##########
-if(ncol(s) >= 8){
+if(ncol(s) >= 22){
   cat("Warning: TOO MANY SAMPLES!! \n")
 }
 
 if(all(elist %in% standards$X1) == F){
-  cat("Error: Unexpected element(s) in the 'elist' vector \n")
-  return()
+  return(cat("Error: Unexpected element(s) in the 'elist' vector \n"))
 }
 ########## Extracting data ##########
 colnames(s) = c("element", colnames(s)[2:ncol(s)])
@@ -91,15 +90,51 @@ for(i in -5:order_max){
   abline(h = 10^(i), lwd=0.2)
 }
 ########## drawing lines ##########
+color_list <- c("black","red","blue","forestgreen","gold","darkviolet","darkorange","skyblue","sienna","hotpink")
+
 for(i in 1:ncol(plotdata)){
-  lines(plotdata[,i], type = "b", pch = 20, col=rainbow(6)[((i - 1) %% 6) + 1])
-  if(((i - 1) %% 6) + 1 == 1){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "red \n")}
-  if(((i - 1) %% 6) + 1 == 2){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "yellow \n")}
-  if(((i - 1) %% 6) + 1 == 3){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "green \n")}
-  if(((i - 1) %% 6) + 1 == 4){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "cyan \n")}
-  if(((i - 1) %% 6) + 1 == 5){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "blue \n")}
-  if(((i - 1) %% 6) + 1 == 6){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "pink \n")}
+  if(i <= 10){
+    lines(plotdata[,i], type = "b", pch = 20, lty=1, col= color_list[i])
+    if(i == 1){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "black \n")}
+    if(i == 2){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "red \n")}
+    if(i == 3){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "blue \n")}
+    if(i == 4){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "green \n")}
+    if(i == 5){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "yellow \n")}
+    if(i == 6){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "violet \n")}
+    if(i == 7){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "orange \n")}
+    if(i == 8){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "skyblue \n")}
+    if(i == 9){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "brown \n")}
+    if(i == 10){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "pink \n")}
+  }
+  if(i > 10 && i <= 20){
+    lines(plotdata[,i], type = "b", pch = 20, lty=2, col= color_list[i])
+    if(i == 11){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "black (dashed)\n")}
+    if(i == 12){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "red (dashed)\n")}
+    if(i == 13){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "blue (dashed)\n")}
+    if(i == 14){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "green (dashed)\n")}
+    if(i == 15){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "yellow (dashed)\n")}
+    if(i == 16){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "violet (dashed)\n")}
+    if(i == 17){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "orange (dashed)\n")}
+    if(i == 18){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "skyblue (dashed)\n")}
+    if(i == 19){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "brown (dashed)\n")}
+    if(i == 20){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "pink (dashed)\n")}
+  }
+  if(i > 20){
+    lines(plotdata[,i], type = "b", pch = 20, col= "grey60")
+    cat("Sample",i,"(",colnames(s)[i + 1],")",":", "gray \n")
+  }
+
 }
+
+#for(i in 1:ncol(plotdata)){
+#  lines(plotdata[,i], type = "b", pch = 20, col=rainbow(6)[((i - 1) %% 6) + 1])
+#  if(((i - 1) %% 6) + 1 == 1){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "red \n")}
+#  if(((i - 1) %% 6) + 1 == 2){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "yellow \n")}
+#  if(((i - 1) %% 6) + 1 == 3){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "green \n")}
+#  if(((i - 1) %% 6) + 1 == 4){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "cyan \n")}
+#  if(((i - 1) %% 6) + 1 == 5){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "blue \n")}
+#  if(((i - 1) %% 6) + 1 == 6){cat("Sample",i,"(",colnames(s)[i + 1],")",":", "pink \n")}
+#}
 ########## Data output ##########
 if(output ==T){write.table(plotdata, file = "output.txt")}
 
